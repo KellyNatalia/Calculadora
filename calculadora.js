60d485974d5ee7f231ebea1e3b8a7f7efea088cc
Kelly Natalia <kellynatalia_toro16@utp.edu.co>
2025-09-15 19:31:39 -0500

    Aagregar la función de porcentage

--- calculadora.js
+++ calculadora.js
@@ -14,13 +14,20 @@ function division (numero1, numero2){
     return numero1 / numero2;
    }
 
+   function porcentage (porcentaje, numero){
+    return (porcentaje * numero) / 100;
+   }
 
    const sumaResultado = suma(36,7);
    const restaResultado = resta(10,15);
    const multiplicacionResultado = multiplicacion(7,8);
    const divisionResultado = division(20,4);
+   const porcentageResultado = porcentage(50,100);
 
    console.log(sumaResultado);
    console.log(restaResultado);
    console.log(multiplicacionResultado);
    console.log(divisionResultado);
+   console.log(porcentageResultado);
+
+   
