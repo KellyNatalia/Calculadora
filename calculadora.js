75d3192f42a67f65171edd28f50b679de3d9fb5f
Kelly Natalia <kellynatalia_toro16@utp.edu.co>
2025-09-15 19:21:10 -0500

    creación de funciones matemáticas básicas

---
+++ calculadora.js
@@ -0,0 +1,26 @@
+function suma (numero1, numero2){
+    return numero1 + numero2;
+   }
+
+  function resta (numero1, numero2){
+    return numero1 - numero2;
+   }
+   
+function multiplicacion (numero1, numero2){
+    return numero1 * numero2;
+   }
+
+function division (numero1, numero2){
+    return numero1 / numero2;
+   }
+
+
+   const sumaResultado = suma(36,7);
+   const restaResultado = resta(10,15);
+   const multiplicacionResultado = multiplicacion(7,8);
+   const divisionResultado = division(20,4);
+
+   console.log(sumaResultado);
+   console.log(restaResultado);
+   console.log(multiplicacionResultado);
+   console.log(divisionResultado);
